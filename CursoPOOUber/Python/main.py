eef66940627855088f8f7227bac2e6411798ae00
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-06 14:33:41 -0500

    Clases con herencia creadas

--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,6 +1,9 @@
+from matplotlib.style import use
 from numpy import var
 from car import Car
 from account import Account
+from user import User
+from driver import Driver
 
 if __name__ == '__main__':
   print('Hola mundo')
@@ -8,3 +11,4 @@ if __name__ == '__main__':
   car = Car("AMS234", Account("Andres Herrera","ANDA876"))
   print(vars(car))
   print(vars(car.driver))
+  
